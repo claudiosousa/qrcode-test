7256a936386132810d90f2c81aad38447efa943a
Claudio <claudio.sousa@gmail.com>
2024-05-09 22:02:27 +0200

    qr codes batch

---
+++ batch/app.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            first_qr_code: 91000000,
+            qrcode_numbers: [],
+            qr_codes_rendered: false,
+            nb_qr_codes: 1000
+        };
+    },
+    methods: {
+        generateQrCodeNumbers() {
+            this.first_qr_code = parseInt(this.first_qr_code);
+            let qrcode_numbers = [];
+            for (let i = 0; i < this.nb_qr_codes; i++) {
+                qrcode_numbers.push(i + this.first_qr_code);
+            }
+            this.qrcode_numbers = qrcode_numbers;
+            this.qr_codes_rendered = true;
+        }
+    },
+    components: {
+        QrCode
+    }
+}).mount('#app');
---
+++ js/QrCode.js
@@ -0,0 +1,37 @@
+const QrCode = {
+    props: ['qrnumber'],
+    template: `
+        <div class="qrcode_component">
+            <div class="qrcode_container"></div>
+            <span class="qrnumber">{{qrnumber}}</span>
+        </div>`,
+    renderQrCode() {
+        this.qrcode_value = generateQrCodeFromNumber(this.qrnumber);
+        this.qrcode = new QRCode(this.$el.firstChild, {
+            text: this.qrcode_value,
+            width: 200,
+            height: 200,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.L
+        });
+    },
+    updated() {
+        this.qrcode.clear();
+        this.qrcode_value = generateQrCodeFromNumber(this.qrnumber);
+        this.qrcode.makeCode(this.qrcode_value);
+    },
+    mounted() {
+        this.$options.renderQrCode.apply(this);
+    }
+}
+
+const generateQrCodeFromNumber = qrnumber => {
+    //const qrnumber = (Math.floor(Date.now() / 1000 / 60) + "").padStart(8, "0"),
+    const pre_date = "0904532",
+        start_date = "20221014",
+        end_date = "20601231",
+        post_date = "346653621"
+
+    return qrnumber + pre_date + start_date + end_date + post_date;
+}
